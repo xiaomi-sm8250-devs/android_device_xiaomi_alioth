c27efaf3b7edbdbd00752fce3dd0466eb6082a01
Amine Elassal <71798072+venomous001@users.noreply.github.com>
2021-12-17 22:11:49 -0500

    change vendor path
    *change path vendor/lineage to vendor/xtended to avoid errors while building
    
    Signed off by: venomous001 <amine.elassal@gmail.com>

--- Xtended_alioth.mk
+++ Xtended_alioth.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
